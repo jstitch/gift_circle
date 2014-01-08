ebdeaba5a5fce82b87eca8ac8066f275846ef553
Javier Novoa <jstitch@gmail.com>
2014-01-08 08:30:55 -0600

    Primer prueba, clase GiftCircle

---
+++ giftcircle.py
@@ -0,0 +1,4 @@
+class GiftCircle(object):
+
+    def __init__(self, fname):
+        pass
--- unit.py
+++ unit.py
@@ -3,21 +3,21 @@
 
 import unittest
 
-from GiftCicle import GiftCircle
+from giftcircle import GiftCircle
 
 class GiftCircleTests(unittest.TestCase):
     def test_load_data(self):
         gift_circle = GiftCircle("test_unit.txt")
-        print gift_circle
+        print(gift_circle)
 
 
 if __name__ == '__main__':
     tl = unittest.TestLoader()
     suite = unittest.TestLoader().loadTestsFromTestCase(GiftCircleTests)
     testcases = tl.getTestCaseNames(GiftCircleTests)
-    print "Testcases:"
+    print("Testcases:")
     for t in testcases:
-        print " ", t
-    print ""
+        print(" ", t)
+    print("")
 
     unittest.TextTestRunner(verbosity=2).run(suite)
