bdc64ec639b5bb6d6ec7636cad5cd3bef056137a
Javier Novoa <jstitch@gmail.com>
2014-01-08 08:34:45 -0600

    Segunda prueba, load data from file

--- giftcircle.py
+++ giftcircle.py
@@ -1,11 +1,10 @@
-# coding: utf-8
-
 class GiftCircle(object):
 
     def __init__(self, fname):
-        self.data = """Javier Novoa Cataño,jstitch@gmail.com
-Javier Naranjamecanica,naranjamecanica00@hotmail.com
-"""
+        data = ""
+        with open(fname,"r") as f:
+            data = f.read()
+        self.data = data
 
     def __str__(self):
         return self.data
