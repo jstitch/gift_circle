cde8c05ead5e05332ce000e6255be1faedf80b21
Javier Novoa <jstitch@gmail.com>
2014-01-08 08:29:21 -0600

    Setup de unittest

---
+++ unit.py
@@ -0,0 +1,23 @@
+#!/usr/bin/pyrg
+# coding: utf-8
+
+import unittest
+
+from GiftCicle import GiftCircle
+
+class GiftCircleTests(unittest.TestCase):
+    def test_load_data(self):
+        gift_circle = GiftCircle("test_unit.txt")
+        print gift_circle
+
+
+if __name__ == '__main__':
+    tl = unittest.TestLoader()
+    suite = unittest.TestLoader().loadTestsFromTestCase(GiftCircleTests)
+    testcases = tl.getTestCaseNames(GiftCircleTests)
+    print "Testcases:"
+    for t in testcases:
+        print " ", t
+    print ""
+
+    unittest.TextTestRunner(verbosity=2).run(suite)
